b841bbc7ef9731aabba53bc9b82e83c4369a1057
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-10-02 10:08:14 -0700

    Silent Mode
    Added toggle to mute all sounds while driving for a completely quiet openpilot driving experience.

--- selfdrive/ui/soundd/sound.cc
+++ selfdrive/ui/soundd/sound.cc
@@ -17,6 +17,8 @@ Sound::Sound(QObject *parent) : sm({"controlsState", "microphone"}) {
   qInfo() << "default audio device: " << QAudioDeviceInfo::defaultOutputDevice().deviceName();
 
   // FrogPilot variables
+  isSilentMode = params.getBool("SilentMode");
+
   isCustomTheme = params.getBool("CustomTheme");
   customSounds = isCustomTheme ? params.getInt("CustomSounds") : 0;
 
@@ -37,7 +39,7 @@ Sound::Sound(QObject *parent) : sm({"controlsState", "microphone"}) {
       assert(s->status() != QSoundEffect::Error);
     });
     s->setSource(QUrl::fromLocalFile(soundPaths[customSounds] + "/" + fn));
-    s->setVolume(volume);
+    s->setVolume(isSilentMode ? 0 : volume);
     sounds[alert] = {s, loops};
   }
 
@@ -55,7 +57,7 @@ void Sound::update() {
     for (auto &[alert, fn, loops, volume] : sound_list) {
       auto &[s, _] = sounds[alert];
       s->setSource(QUrl::fromLocalFile(soundPaths[customSounds] + "/" + fn));
-      s->setVolume(volume);
+      s->setVolume(isSilentMode ? 0 : volume);
     }
   }
 
