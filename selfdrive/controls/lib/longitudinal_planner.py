948e5de2b331a42f31916068a27a2187a114132e
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-10-16 09:06:40 -0700

    Custom driving personality profiles
    Added toggles to customize the driving personality profiles.

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -107,6 +107,14 @@ class LongitudinalPlanner:
     self.acceleration_profile = self.params.get_int("AccelerationProfile") if self.longitudinal_tuning else 2
     self.aggressive_acceleration = self.params.get_bool("AggressiveAcceleration") and self.longitudinal_tuning
 
+    self.custom_personalities = self.params.get_bool("CustomPersonalities")
+    self.aggressive_follow = self.params.get_int("AggressivePersonality") / 10
+    self.standard_follow = self.params.get_int("StandardPersonality") / 10
+    self.relaxed_follow = self.params.get_int("RelaxedPersonality") / 10
+    self.aggressive_jerk = self.params.get_int("AggressiveJerk") / 10
+    self.standard_jerk = self.params.get_int("StandardJerk") / 10
+    self.relaxed_jerk = self.params.get_int("RelaxedJerk") / 10
+
     self.update_frogpilot_params()
     # Set variables for Conditional Experimental Mode
     self.conditional_experimental_mode = self.params.get_bool("ConditionalExperimental")
@@ -203,11 +211,12 @@ class LongitudinalPlanner:
     accel_limits_turns[0] = min(accel_limits_turns[0], self.a_desired + 0.05)
     accel_limits_turns[1] = max(accel_limits_turns[1], self.a_desired - 0.05)
 
-    self.mpc.set_weights(prev_accel_constraint, personality=self.personality)
+    self.mpc.set_weights(prev_accel_constraint, self.custom_personalities, self.aggressive_jerk, self.standard_jerk, self.relaxed_jerk, personality=self.personality)
     self.mpc.set_accel_limits(accel_limits_turns[0], accel_limits_turns[1])
     self.mpc.set_cur_state(self.v_desired_filter.x, self.a_desired)
     x, v, a, j = self.parse_model(sm['modelV2'], self.v_model_error)
-    self.mpc.update(sm['radarState'], v_cruise, x, v, a, j, self.aggressive_acceleration, personality=self.personality)
+    self.mpc.update(sm['radarState'], v_cruise, x, v, a, j, self.aggressive_acceleration, 
+                    self.custom_personalities, self.aggressive_follow, self.standard_follow, self.relaxed_follow, personality=self.personality)
 
     self.x_desired_trajectory_full = np.interp(T_IDXS, T_IDXS_MPC, self.mpc.x_solution)
     self.v_desired_trajectory_full = np.interp(T_IDXS, T_IDXS_MPC, self.mpc.v_solution)
@@ -383,3 +392,12 @@ class LongitudinalPlanner:
       self.signal = self.params.get_bool("ConditionalSignal")
       self.slower_lead = self.params.get_bool("ConditionalSlowerLead")
       self.stop_lights = self.params.get_bool("ConditionalStopLights")
+
+    self.custom_personalities = self.params.get_bool("CustomPersonalities")
+    if self.custom_personalities:
+      self.aggressive_follow = self.params.get_int("AggressivePersonality") / 10
+      self.standard_follow = self.params.get_int("StandardPersonality") / 10
+      self.relaxed_follow = self.params.get_int("RelaxedPersonality") / 10
+      self.aggressive_jerk = self.params.get_int("AggressiveJerk") / 10
+      self.standard_jerk = self.params.get_int("StandardJerk") / 10
+      self.relaxed_jerk = self.params.get_int("RelaxedJerk") / 10
