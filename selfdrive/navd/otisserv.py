f62554ddd309fb29754f5dd9c6ad18050d62f537
mike8643 <nguyen95465@yahoo.com>
2023-11-15 20:40:15 -0500

    Web server NoO Driving Directions

--- selfdrive/navd/otisserv.py
+++ selfdrive/navd/otisserv.py
@@ -50,6 +50,12 @@ class OtisServ(BaseHTTPRequestHandler):
     if self.path == '/logo.png':
       self.get_logo()
       return
+    if self.path == '/navdirections.json':
+      self.get_navdirections()
+      return
+    if self.path == '/CurrentStep.json':
+      self.get_currentstep()
+      return
     if self.path == '/?reset=1':
       params.put("NavDestination", "")
     if use_amap:
@@ -110,7 +116,12 @@ class OtisServ(BaseHTTPRequestHandler):
       if self.get_app_token() is None:
         self.display_page_app_token()
         return
-      self.display_page_addr_input()
+      if params.get_int("PrimeType") != 0:
+        self.display_prime_directions()
+      elif params.get("NavDestination") is not None:
+        self.display_nav_directions()
+      else :
+        self.display_page_addr_input() 
 
   def do_POST(self):
     use_amap = params.get_bool('EnableAmap')
@@ -215,13 +226,32 @@ class OtisServ(BaseHTTPRequestHandler):
     elif use_gmap:
       self.display_page_gmap()
     else:
-      self.display_page_addr_input()
+      if params.get("NavDestination") is not None:
+        self.display_nav_directions()
+      else :
+        self.display_page_addr_input()
 
   def get_logo(self):
     self.send_response(200)
     self.send_header('Content-type','image/png')
     self.end_headers()
     f = open("%s/selfdrive/assets/img_spinner_comma.png" % BASEDIR, "rb")
+    self.wfile.write(f.read())
+    f.close()
+
+  def get_navdirections(self):
+    self.send_response(200)
+    self.send_header('Content-type','application/json')
+    self.end_headers()
+    f = open("%s/selfdrive/manager/navdirections.json" % BASEDIR, "rb")
+    self.wfile.write(f.read())
+    f.close()
+
+  def get_currentstep(self):
+    self.send_response(200)
+    self.send_header('Content-type','application/json')
+    self.end_headers()
+    f = open("%s/selfdrive/manager/CurrentStep.json" % BASEDIR, "rb")
     self.wfile.write(f.read())
     f.close()
 
@@ -305,9 +335,17 @@ class OtisServ(BaseHTTPRequestHandler):
 
   def display_page_addr_input(self, msg = ""):
     self.wfile.write(bytes(self.get_parsed_template("body", {"{{content}}": self.get_parsed_template("addr_input", {"{{msg}}": msg})}), "utf-8"))
+    
+  def display_nav_directions(self, msg = ""):
+    content = self.get_parsed_template("addr_input", {"{{msg}}": ""}) + self.get_parsed_template("nav_directions", {"{{msg}}": msg})
+    self.wfile.write(bytes(self.get_parsed_template("body", {"{{content}}": content }), "utf-8"))
 
   def display_page_nav_confirmation(self, addr, lon, lat):
     content = self.get_parsed_template("addr_input", {"{{msg}}": ""}) + self.get_parsed_template("nav_confirmation", {"{{token}}": self.get_public_token(), "{{lon}}": lon, "{{lat}}": lat, "{{addr}}": addr})
+    self.wfile.write(bytes(self.get_parsed_template("body", {"{{content}}": content }), "utf-8"))
+
+  def display_prime_directions(self, msg = ""):
+    content = self.get_parsed_template("nav_directions", {"{{msg}}": msg})
     self.wfile.write(bytes(self.get_parsed_template("body", {"{{content}}": content }), "utf-8"))
 
   def display_page_gmap(self):
