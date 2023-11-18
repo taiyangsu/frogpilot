tysonseable/openpilot

Conditional Experimental Mode
Added toggles for "Conditional Experimental Mode".

Conditions based on road curvature, turn signals, speed, lead speed, navigation instructions, and stop signs/stop lights are all individually toggleable.

selfdrive/ui/qt/offroad/frogpilot_settings.cc
@@ -14,6 +14,7 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(QWidget *parent) : FrogPilotPanel
 
   static const std::vector<std::tuple<QString, QString, QString, QString>> toggles = {
     {"AlwaysOnLateral", "Always on Lateral / No disengage on Brake Pedal", "Keep openpilot lateral control when using either the brake or gas pedals. openpilot is only disengaged by deactivating the 'Cruise Control' button.", "../assets/offroad/icon_always_on_lateral.png"},
+    {"ConditionalExperimental", "Conditional Experimental Mode", "Automatically activate 'Experimental Mode' based on specified conditions.", "../assets/offroad/icon_conditional.png"},
     {"LateralTune", "Lateral Tuning", "Change the way openpilot steers.", "../assets/offroad/icon_lateral_tune.png"},
     {"LongitudinalTune", "Longitudinal Tuning", "Change the way openpilot accelerates and brakes.", "../assets/offroad/icon_longitudinal_tune.png"},
   };
@@ -24,6 +25,20 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(QWidget *parent) : FrogPilotPanel
       createSubControl(key, label, desc, icon, {}, {
         {"AlwaysOnLateralMain", "Enable AOL On Cruise Main", "Enables Always On Lateral by simply turning on cruise control as opposed to requiring openpilot to be enabled first."}
       });
+    } else if (key == "ConditionalExperimental") {
+      createSubControl(key, label, desc, icon, {
+        createDualParamControl(new ConditionalSpeed(), new ConditionalSpeedLead()),
+      });
+      createSubButtonControl(key, {
+        {"CECurves", "Curves"},
+        {"CECurvesLead", "Curves With Lead"},
+        {"CENavigation", "Navigation Based"}
+      }, mainLayout);
+      createSubButtonControl(key, {
+        {"CESlowerLead", "Slower Lead Ahead"},
+        {"CEStopLights", "Stop Lights and Stop Signs"},
+        {"CESignal", "Turn Signal < " + QString(isMetric ? "90kph" : "55mph")}
+      }, mainLayout);
     } else if (key == "LateralTune") {
       createSubControl(key, label, desc, icon, {}, {
         {"AverageCurvature", "Average Desired Curvature", "Use Pfeiferj's distance based curvature adjustment for smoother handling of curves."},
@@ -148,6 +163,10 @@ ParamControl *FrogPilotPanel::createParamControl(const QString &key, const QStri
       for (QWidget *widget : it->second) {
         widget->setVisible(state);
       }
+    }
+
+    if (key == "ConditionalExperimental") {
+      params.putBoolNonBlocking("ExperimentalMode", state);
     }
   });
   return control;

selfdrive/ui/qt/offroad/frogpilot_settings.h
@@ -187,3 +187,15 @@ ParamController(AccelerationProfile, "AccelerationProfile", "   Acceleration Pro
   return profile == 1 ? "Eco" : profile == 2 ? "Normal" : "Sport";,
   return std::clamp(v, 1, 3);
 )
+
+ParamController(ConditionalSpeed, "ConditionalSpeed", "Below", "Switch to 'Experimental Mode' below this speed when there is no lead vehicle.", "../assets/offroad/icon_blank.png",
+  const int speed = params.getInt("ConditionalSpeed");
+  return speed == 0 ? "Off" : QString::number(speed) + (isMetric ? " kph" : " mph");,
+  return std::clamp(v, 0, isMetric ? 150 : 99);
+)
+
+ParamController(ConditionalSpeedLead, "ConditionalSpeedLead", "With Lead", "Switch to 'Experimental Mode' below this speed when there is a lead vehicle.", "../assets/offroad/icon_blank.png",
+  const int speedLead = params.getInt("ConditionalSpeedLead");
+  return speedLead == 0 ? "Off" : QString::number(speedLead) + (isMetric ? " kph" : " mph");,
+  return std::clamp(v, 0, isMetric ? 150 : 99);
+)

taiyangsu/frogpilot

selfdrive/ui/qt/offroad/frogpilot_settings.cc
@@ -14,6 +14,7 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(QWidget *parent) : FrogPilotPanel
 
   static const std::vector<std::tuple<QString, QString, QString, QString>> toggles = {
     {"AlwaysOnLateral", "Always on Lateral / No disengage on Brake Pedal", "Keep openpilot lateral control when using either the brake or gas pedals. openpilot is only disengaged by deactivating the 'Cruise Control' button.", "../assets/offroad/icon_always_on_lateral.png"},
+    {"ConditionalExperimental", "Conditional Experimental Mode", "Automatically activate 'Experimental Mode' based on specified conditions.", "../assets/offroad/icon_conditional.png"},
     {"LateralTune", "Lateral Tuning", "Change the way openpilot steers.", "../assets/offroad/icon_lateral_tune.png"},
     {"LongitudinalTune", "Longitudinal Tuning", "Change the way openpilot accelerates and brakes.", "../assets/offroad/icon_longitudinal_tune.png"},
   };
@@ -24,6 +25,20 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(QWidget *parent) : FrogPilotPanel
       createSubControl(key, label, desc, icon, {}, {
         {"AlwaysOnLateralMain", "Enable AOL On Cruise Main", "Enables Always On Lateral by simply turning on cruise control as opposed to requiring openpilot to be enabled first."}
       });
+    } else if (key == "ConditionalExperimental") {
+      createSubControl(key, label, desc, icon, {
+        createDualParamControl(new CESpeed(), new CESpeedLead()),
+      });
+      createSubButtonControl(key, {
+        {"CECurves", "Curves"},
+        {"CECurvesLead", "Curves With Lead"},
+        {"CENavigation", "Navigation Based"}
+      }, mainLayout);
+      createSubButtonControl(key, {
+        {"CESlowerLead", "Slower Lead Ahead"},
+        {"CEStopLights", "Stop Lights and Stop Signs"},
+        {"CESignal", "Turn Signal < " + QString(isMetric ? "90kph" : "55mph")}
+      }, mainLayout);
     } else if (key == "LateralTune") {
       createSubControl(key, label, desc, icon, {}, {
         {"AverageCurvature", "Average Desired Curvature", "Use Pfeiferj's distance based curvature adjustment for smoother handling of curves."},
@@ -151,6 +166,10 @@ ParamControl *FrogPilotPanel::createParamControl(const QString &key, const QStri
       for (QWidget *widget : it->second) {
         widget->setVisible(state);
       }
+    }
+
+    if (key == "ConditionalExperimental") {
+      params.putBoolNonBlocking("ExperimentalMode", state);
     }
   });
   return control;

selfdrive/ui/qt/offroad/frogpilot_settings.h
@@ -162,7 +162,7 @@ public: \
     if (std::string(#className) == "CameraView" || std::string(#className) == "DeviceShutdown" || std::string(#className) == "StoppingDistance" || std::string(#className) == "WheelIcon") { \
       label.setFixedWidth(225); \
     } \
-    if (std::string(#className) == "ConditionalSpeed" || std::string(#className) == "ConditionalSpeedLead") { \
+    if (std::string(#className) == "CESpeed" || std::string(#className) == "CESpeedLead") { \
       label.setFixedWidth(180); \
     } \
     refresh(); \
@@ -192,4 +192,16 @@ ParamController(CameraView, "CameraView", "Camera View (Cosmetic Only)", "Set yo
   const int camera = params.getInt("CameraView");
   return camera == 0 ? "Auto" : camera == 1 ? "Standard" : camera == 2 ? "Wide" : "Driver";,
   return v >= 0 ? v % 4 : 3;
+)
+
+ParamController(CESpeed, "CESpeed", "Below", "Switch to 'Experimental Mode' below this speed when there is no lead vehicle.", "../assets/offroad/icon_blank.png",
+  const int speed = params.getInt("CESpeed");
+  return speed == 0 ? "Off" : QString::number(speed) + (isMetric ? " kph" : " mph");,
+  return std::clamp(v, 0, isMetric ? 150 : 99);
+)
+
+ParamController(CESpeedLead, "CESpeedLead", "With Lead", "Switch to 'Experimental Mode' below this speed when there is a lead vehicle.", "../assets/offroad/icon_blank.png",
+  const int speedLead = params.getInt("CESpeedLead");
+  return speedLead == 0 ? "Off" : QString::number(speedLead) + (isMetric ? " kph" : " mph");,
+  return std::clamp(v, 0, isMetric ? 150 : 99);
 )