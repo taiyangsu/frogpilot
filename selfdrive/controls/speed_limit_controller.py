tysonseable/openpilot

Speed limit controller
Added toggle to control the cruise set speed according to speed limit supplied by OSM, NOO, or the vehicle itself.

Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>
Co-Authored-By: Efini <19368997+efini@users.noreply.github.com>
Co-Authored-By: Kumar <36933347+rav4kumar@users.noreply.github.com>

selfdrive/controls/speed_limit_controller.py
@@ -1,0 +1,110 @@
+from openpilot.common.conversions import Conversions as CV
+from openpilot.common.params import Params
+import json
+
+params = Params()
+params_memory = Params("/dev/shm/params")
+
+class SpeedLimitController:
+  car_speed_limit: float = 0  # m/s
+  map_speed_limit: float = 0  # m/s
+  nav_speed_limit: float = 0  # m/s
+  prv_speed_limit: float = 0  # m/s
+
+  def __init__(self) -> None:
+    self.update_frogpilot_params()
+    self.write_map_state()
+    self.write_nav_state()
+
+  def update_current_max_velocity(self, car_speed_limit: float, max_v: float, load_state: bool = True, write_state: bool = True) -> None:
+    self.car_speed_limit = car_speed_limit
+    if load_state:
+      self.load_state()
+
+  @property
+  def offset(self) -> float:
+    if self.speed_limit < 15.6464:
+      return self.offset1
+    elif self.speed_limit < 24.5872:
+      return self.offset2
+    elif self.speed_limit < 29.0576:
+      return self.offset3
+    else:
+      return self.offset4
+
+  @property
+  def speed_limit(self) -> float:
+    params_memory.put_bool("SLCExperimentalMode", False)
+
+    limits = [self.car_speed_limit, self.map_speed_limit, self.nav_speed_limit]
+    filtered_limits = [limit for limit in limits if limit > 0]
+
+    if self.highest and filtered_limits:
+      return max(filtered_limits)
+    if self.lowest and filtered_limits:
+      return min(filtered_limits)
+
+    priority_orders = [
+      ["nav", "car", "map"],
+      ["nav", "map", "car"],
+      ["nav", "map"],
+      ["nav", "car"],
+      ["nav"],
+      ["map", "car", "nav"],
+      ["map", "nav", "car"],
+      ["map", "nav"],
+      ["map", "car"],
+      ["map"],
+      ["car", "nav", "map"],
+      ["car", "map", "nav"],
+      ["car", "map"],
+      ["car", "nav"],
+      ["car"]
+    ]
+
+    if self.speed_limit_priority < len(priority_orders):
+      for source in priority_orders[self.speed_limit_priority]:
+        limit = getattr(self, f"{source}_speed_limit", 0)
+        if limit > 0:
+          self.prv_speed_limit = limit
+          return limit
+
+    if self.use_experimental_mode:
+      params_memory.put_bool("SLCExperimentalMode", True)
+      return 0
+    elif self.use_previous_limit:
+      return self.prv_speed_limit
+
+    return 0
+
+  @property
+  def desired_speed_limit(self):
+    return self.speed_limit + self.offset if self.speed_limit else 0
+
+  def load_state(self):
+    self.nav_speed_limit = json.loads(params_memory.get("NavSpeedLimit"))
+    self.map_speed_limit = json.loads(params_memory.get("MapSpeedLimit"))
+
+  def write_map_state(self):
+    params_memory.put("MapSpeedLimit", json.dumps(self.map_speed_limit))
+
+  def write_nav_state(self):
+    params_memory.put("NavSpeedLimit", json.dumps(self.nav_speed_limit))
+
+  def update_frogpilot_params(self):
+    conversion = CV.KPH_TO_MS if params.get_bool("IsMetric") else CV.MPH_TO_MS
+
+    self.offset1 = params.get_int("Offset1") * conversion
+    self.offset2 = params.get_int("Offset2") * conversion
+    self.offset3 = params.get_int("Offset3") * conversion
+    self.offset4 = params.get_int("Offset4") * conversion
+
+    self.highest = params.get_int("SLCPriority") == 15
+    self.lowest = params.get_int("SLCPriority") == 16
+    self.speed_limit_priority = params.get_int("SLCPriority")
+
+    slc_fallback = params.get_int("SLCFallback")
+    self.use_experimental_mode = slc_fallback == 1
+    self.use_previous_limit = slc_fallback == 2
+
+SpeedLimitController = SpeedLimitController()

selfdrive/ui/qt/offroad/frogpilot_settings.h
@@ -189,10 +189,13 @@ class className : public ParamValueControl { \
   Q_OBJECT \
 public: \
   className() : ParamValueControl(labelText, descText, iconPath) { \
+    if (std::string(#className) == "SLCFallback" || std::string(#className) == "SLCPriority") { \
+      label.setFixedWidth(750); \
+    } \
     if (std::string(#className) == "DeviceShutdown" || std::string(#className) == "StoppingDistance" || std::string(#className) == "WheelIcon") { \
       label.setFixedWidth(225); \
     } \
-    if (std::string(#className) == "ConditionalSpeed" || std::string(#className) == "ConditionalSpeedLead") { \
+    if (std::string(#className) == "ConditionalSpeed" || std::string(#className) == "ConditionalSpeedLead" || std::string(#className) == "Offset1" || std::string(#className) == "Offset2" || std::string(#className) == "Offset3" || std::string(#className) == "Offset4") { \
       label.setFixedWidth(180); \
     } \
     refresh(); \
@@ -291,6 +294,46 @@ ParamController(Model, "Model", "Model Selector (Requires Reboot)", "Select your
   return v >= 0 ? v % 2 : 1;
 )
 
+ParamController(Offset1, "Offset1", "  0-34", "Set the speed limit offset when the speed limit is between 0 and 34 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset1")) + " mph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset1Metric, "Offset1", "  0-54", "Set the speed limit offset when the speed limit is between 0 and 34 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset1")) + " kph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset2, "Offset2", "35-54", "Set the speed limit offset when the speed limit is between 35 and 54 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset2")) + " mph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset2Metric, "Offset2", "55-89", "Set the speed limit offset when the speed limit is between 35 and 54 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset2")) + " kph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset3, "Offset3", "55-64", "Set the speed limit offset when the speed limit is between 55 and 64 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset3")) + " mph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset3Metric, "Offset3", "90-104", "Set the speed limit offset when the speed limit is between 55 and 64 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset3")) + " kph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset4, "Offset4", "65-99", "Set the speed limit offset when the speed limit is between 65 and 99 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset4")) + " mph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset4Metric, "Offset4", "105-159", "Set the speed limit offset when the speed limit is between 65 and 99 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset4")) + " kph";,
+  return std::clamp(v, 0, 99);
+)
+
 ParamController(PathEdgeWidth, "PathEdgeWidth", "Path Edges", "Customize the path edge width that displays current driving statuses.\n\nDefault is 20% of the total path.\n\nBlue = Navigation\nLight Blue = Always On Lateral\nGreen = Default with 'FrogPilot Colors'\nLight Green = Default with stock colors\nOrange = Experimental Mode Active\nYellow = Conditional Overriden", "../assets/offroad/icon_blank.png",
   return QString::number(params.getInt("PathEdgeWidth")) + "%";,
   return std::clamp(v, 0, 100);
@@ -320,6 +363,34 @@ ParamController(ScreenBrightness, "ScreenBrightness", "Screen Brightness", "Set 
   const int brightness = params.getInt("ScreenBrightness");
   return brightness == 101 ? "Auto" : brightness == 0 ? "Off" : QString::number(brightness) + "%";,
   return std::clamp(v, 0, 101);
+)
+
+ParamController(SLCFallback, "SLCFallback", "SLC Fallback", "Set your preferred fallback method for when there's no speed limit found in either Navigation, OSM, or the car's dashboard.", "../assets/offroad/icon_blank.png",
+  const int fallback = params.getInt("SLCFallback");
+  return fallback == 0 ? "None" : fallback == 1 ? "Experimental Mode" : "Previous Speed Limit";,
+  return v >= 0 ? v % 3 : 2;
+)
+
+ParamController(SLCPriority, "SLCPriority", "SLC Priority", "Set your preferred priority order when deciding what speed limit to use for Speed Limit Controller.", "../assets/offroad/icon_blank.png",
+  const int priority = params.getInt("SLCPriority");
+  return priority == 0 ? "Navigation, Dash, OSM" : 
+         priority == 1 ? "Navigation, OSM, Dash" : 
+         priority == 2 ? "Navigation, OSM" : 
+         priority == 3 ? "Navigation, Dash" : 
+         priority == 4 ? "Navigation" : 
+         priority == 5 ? "OSM, Dash, Navigation" : 
+         priority == 6 ? "OSM, Navigation, Dash" : 
+         priority == 7 ? "OSM, Navigation" : 
+         priority == 8 ? "OSM, Dash" : 
+         priority == 9 ? "OSM" : 
+         priority == 10 ? "Dash, Navigation, OSM" : 
+         priority == 11 ? "Dash, OSM, Navigation" : 
+         priority == 12 ? "Dash, OSM" : 
+         priority == 13 ? "Dash, Navigation" : 
+         priority == 14 ? "Dash" : 
+         priority == 15 ? "Highest" : 
+         "Lowest";,
+  return v >= 0 ? v % 17 : 16;
 )
 
 ParamController(StandardJerk, "StandardJerk", "Jerk Value", "Set the jerk value for the 'Standard Personality'.\n\nValue represents the responsiveness of the brake/gas pedals.\n\nHigher value = Less responsive/more 'relaxed'\n\nStock has a value of 1.0.", "../assets/offroad/icon_blank.png",

selfdrive/ui/qt/onroad.cc
@@ -125,16 +125,23 @@ void OnroadWindow::mousePressEvent(QMouseEvent* e) {
   const QRect speedRect(rect().center().x() - 175, 50, 350, 350);
   const bool isSpeedClicked = speedRect.contains(e->pos());
 
-  if (isMaxSpeedClicked || isSpeedClicked) {
+  // Speed limit offset button
+  const QRect speedLimitRect(7, 250, 225, 225);
+  const bool isSpeedLimitClicked = speedLimitRect.contains(e->pos());
+
+  if (isMaxSpeedClicked || isSpeedClicked || isSpeedLimitClicked) {
     // Check if the click was within the max speed area
     if (isMaxSpeedClicked) {
       reverseCruise = !params.getBool("ReverseCruise");
       params.putBoolNonBlocking("ReverseCruise", reverseCruise);
       paramsMemory.putBoolNonBlocking("FrogPilotTogglesUpdated", true);
     // Check if the click was within the speed text area
-    } else {
+    } else if (isSpeedClicked) {
       speedHidden = !params.getBool("HideSpeed");
       params.putBoolNonBlocking("HideSpeed", speedHidden);
+    } else {
+      displaySLCOffset = !params.getBool("DisplaySLCOffset");
+      params.putBoolNonBlocking("DisplaySLCOffset", displaySLCOffset);
     }
     widgetClicked = true;
   // If the click wasn't for anything specific, change the value of "ExperimentalMode"
@@ -460,6 +467,9 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
   if (params.getBool("ReverseCruise")) {
     reverseCruise = true;
   }
+  if (params.getBool("DisplaySLCOffset")) {
+    displaySLCOffset = true;
+  }
 
   // Load miscellaneous images
   compass_inner_img = loadPixmap("../assets/images/compass_inner.png", {img_size, img_size});
@@ -511,10 +521,13 @@ void AnnotatedCameraWidget::updateState(const UIState &s) {
   speed *= s.scene.is_metric ? MS_TO_KPH : MS_TO_MPH;
 
   auto speed_limit_sign = nav_instruction.getSpeedLimitSign();
-  speedLimit = nav_alive ? nav_instruction.getSpeedLimit() : 0.0;
+  speedLimit = slcSpeedLimit ? slcSpeedLimit : nav_alive ? nav_instruction.getSpeedLimit() : 0.0;
   speedLimit *= (s.scene.is_metric ? MS_TO_KPH : MS_TO_MPH);
+  if (slcSpeedLimit) {
+    speedLimit = std::round(speedLimit - (displaySLCOffset ? slcSpeedLimitOffset : 0));
+  }
 
-  has_us_speed_limit = (nav_alive && speed_limit_sign == cereal::NavInstruction::SpeedLimitSign::MUTCD);
+  has_us_speed_limit = (nav_alive && speed_limit_sign == cereal::NavInstruction::SpeedLimitSign::MUTCD) || slcSpeedLimit;
   has_eu_speed_limit = (nav_alive && speed_limit_sign == cereal::NavInstruction::SpeedLimitSign::VIENNA);
   is_metric = s.scene.is_metric;
   speedUnit =  s.scene.is_metric ? tr("km/h") : tr("mph");
@@ -562,6 +575,9 @@ void AnnotatedCameraWidget::updateState(const UIState &s) {
   obstacleDistanceStock = scene.obstacle_distance_stock;
   roadNameUI = scene.road_name_ui;
   showDriverCamera = scene.show_driver_camera;
+  slcOverridden = scene.speed_limit_overridden;
+  slcSpeedLimit = scene.speed_limit;
+  slcSpeedLimitOffset = scene.speed_limit_offset * (is_metric ? MS_TO_KPH : MS_TO_MPH);
   stoppedEquivalence = scene.stopped_equivalence;
   stoppedEquivalenceStock = scene.stopped_equivalence_stock;
   turnSignalLeft = scene.turn_signal_left;
@@ -604,6 +620,7 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
   p.fillRect(0, 0, width(), UI_HEADER_HEIGHT, bg);
 
   QString speedLimitStr = (speedLimit > 1) ? QString::number(std::nearbyint(speedLimit)) : "–";
+  QString speedLimitOffsetStr = (slcSpeedLimitOffset > 1) ? "+" + QString::number(std::nearbyint(slcSpeedLimitOffset)) : "–";
   QString speedStr = QString::number(std::nearbyint(speed));
   QString setSpeedStr = is_cruise_set ? QString::number(std::nearbyint(setSpeed)) : "–";
 
@@ -667,11 +684,23 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
     p.setPen(QPen(blackColor(), 6));
     p.drawRoundedRect(sign_rect.adjusted(9, 9, -9, -9), 16, 16);
 
-    p.setFont(InterFont(28, QFont::DemiBold));
-    p.drawText(sign_rect.adjusted(0, 22, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("SPEED"));
-    p.drawText(sign_rect.adjusted(0, 51, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("LIMIT"));
-    p.setFont(InterFont(70, QFont::Bold));
-    p.drawText(sign_rect.adjusted(0, 85, 0, 0), Qt::AlignTop | Qt::AlignHCenter, speedLimitStr);
+    p.save();
+    p.setOpacity(slcOverridden ? 0.25 : 1.0);
+    if (displaySLCOffset) {
+      p.setFont(InterFont(28, QFont::DemiBold));
+      p.drawText(sign_rect.adjusted(0, 22, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("LIMIT"));
+      p.setFont(InterFont(70, QFont::Bold));
+      p.drawText(sign_rect.adjusted(0, 51, 0, 0), Qt::AlignTop | Qt::AlignHCenter, speedLimitStr);
+      p.setFont(InterFont(50, QFont::DemiBold));
+      p.drawText(sign_rect.adjusted(0, 120, 0, 0), Qt::AlignTop | Qt::AlignHCenter, speedLimitOffsetStr);
+    } else {
+      p.setFont(InterFont(28, QFont::DemiBold));
+      p.drawText(sign_rect.adjusted(0, 22, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("SPEED"));
+      p.drawText(sign_rect.adjusted(0, 51, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("LIMIT"));
+      p.setFont(InterFont(70, QFont::Bold));
+      p.drawText(sign_rect.adjusted(0, 85, 0, 0), Qt::AlignTop | Qt::AlignHCenter, speedLimitStr);
+    }
+    p.restore();
   }
 
   // EU (Vienna style) sign

taiyangsu/frogpilot

selfdrive/controls/speed_limit_controller.py
@@ -1,0 +1,115 @@
+from openpilot.common.conversions import Conversions as CV
+from openpilot.common.params import Params
+import json
+
+params = Params()
+params_memory = Params("/dev/shm/params")
+
+class SpeedLimitController:
+  car_speed_limit: float = 0  # m/s
+  lst_speed_limit: float = 0  # m/s
+  map_speed_limit: float = 0  # m/s
+  nav_speed_limit: float = 0  # m/s
+
+  def __init__(self) -> None:
+    self.update_frogpilot_params()
+    self.write_map_state()
+    self.write_nav_state()
+
+  def update_current_max_velocity(self, car_speed_limit: float, max_v: float, load_state: bool = True, write_state: bool = True) -> None:
+    self.car_speed_limit = car_speed_limit
+    if load_state:
+      self.load_state()
+
+  @property
+  def offset(self) -> float:
+    if self.speed_limit < 15.6464:
+      return self.offset1
+    elif self.speed_limit < 24.5872:
+      return self.offset2
+    elif self.speed_limit < 29.0576:
+      return self.offset3
+    else:
+      return self.offset4
+
+  @property
+  def speed_limit(self) -> float:
+    params_memory.put_bool("SLCExperimentalMode", False)
+
+    limits = [self.car_speed_limit, self.map_speed_limit, self.nav_speed_limit]
+    filtered_limits = [limit for limit in limits if limit > 0]
+
+    if self.highest and filtered_limits:
+      return max(filtered_limits)
+    if self.lowest and filtered_limits:
+      return min(filtered_limits)
+
+    priority_orders = [
+      ["nav", "car", "map"],
+      ["nav", "map", "car"],
+      ["nav", "map"],
+      ["nav", "car"],
+      ["nav"],
+      ["map", "car", "nav"],
+      ["map", "nav", "car"],
+      ["map", "nav"],
+      ["map", "car"],
+      ["map"],
+      ["car", "nav", "map"],
+      ["car", "map", "nav"],
+      ["car", "map"],
+      ["car", "nav"],
+      ["car"]
+    ]
+
+    if self.speed_limit_priority < len(priority_orders):
+      for source in priority_orders[self.speed_limit_priority]:
+        limit = getattr(self, f"{source}_speed_limit", 0)
+        if limit > 0:
+          self.prv_speed_limit = limit
+          return limit
+
+    if self.use_experimental_mode:
+      params_memory.put_bool("SLCExperimentalMode", True)
+      return 0
+    elif self.use_previous_limit:
+      if self.lst_speed_limit != self.prv_speed_limit:
+        params.put_int("PreviousSpeedLimit", self.prv_speed_limit * 100)
+      self.lst_speed_limit = self.prv_speed_limit
+      return self.prv_speed_limit
+
+    return 0
+
+  @property
+  def desired_speed_limit(self):
+    return self.speed_limit + self.offset if self.speed_limit else 0
+
+  def load_state(self):
+    self.nav_speed_limit = json.loads(params_memory.get("NavSpeedLimit"))
+    self.map_speed_limit = json.loads(params_memory.get("MapSpeedLimit"))
+
+  def write_map_state(self):
+    params_memory.put("MapSpeedLimit", json.dumps(self.map_speed_limit))
+
+  def write_nav_state(self):
+    params_memory.put("NavSpeedLimit", json.dumps(self.nav_speed_limit))
+
+  def update_frogpilot_params(self):
+    conversion = CV.KPH_TO_MS if params.get_bool("IsMetric") else CV.MPH_TO_MS
+
+    self.offset1 = params.get_int("Offset1") * conversion
+    self.offset2 = params.get_int("Offset2") * conversion
+    self.offset3 = params.get_int("Offset3") * conversion
+    self.offset4 = params.get_int("Offset4") * conversion
+
+    self.highest = params.get_int("SLCPriority") == 15
+    self.lowest = params.get_int("SLCPriority") == 16
+    self.speed_limit_priority = params.get_int("SLCPriority")
+
+    slc_fallback = params.get_int("SLCFallback")
+    self.use_experimental_mode = slc_fallback == 1
+    self.use_previous_limit = slc_fallback == 2
+
+    self.prv_speed_limit = params.get_int("PreviousSpeedLimit") / 100
+
+SpeedLimitController = SpeedLimitController()

selfdrive/ui/qt/offroad/frogpilot_settings.h
@@ -189,10 +189,13 @@ class className : public ParamValueControl { \
   Q_OBJECT \
 public: \
   className() : ParamValueControl(labelText, descText, iconPath) { \
+    if (std::string(#className) == "SLCFallback" || std::string(#className) == "SLCPriority") { \
+      label.setFixedWidth(750); \
+    } \
     if (std::string(#className) == "CameraView" || std::string(#className) == "DeviceShutdown" || std::string(#className) == "StoppingDistance" || std::string(#className) == "WheelIcon") { \
       label.setFixedWidth(225); \
     } \
-    if (std::string(#className) == "CESpeed" || std::string(#className) == "CESpeedLead") { \
+    if (std::string(#className) == "CESpeed" || std::string(#className) == "CESpeedLead" || std::string(#className) == "Offset1" || std::string(#className) == "Offset2" || std::string(#className) == "Offset3" || std::string(#className) == "Offset4") { \
       label.setFixedWidth(180); \
     } \
     refresh(); \
@@ -297,6 +300,46 @@ ParamController(Model, "Model", "Model Selector (Requires Reboot)", "Select your
   return v >= 0 ? v % 3 : 2;
 )
 
+ParamController(Offset1, "Offset1", "  0-34", "Set the speed limit offset when the speed limit is between 0 and 34 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset1")) + " mph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset1Metric, "Offset1", "  0-54", "Set the speed limit offset when the speed limit is between 0 and 34 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset1")) + " kph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset2, "Offset2", "35-54", "Set the speed limit offset when the speed limit is between 35 and 54 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset2")) + " mph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset2Metric, "Offset2", "55-89", "Set the speed limit offset when the speed limit is between 35 and 54 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset2")) + " kph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset3, "Offset3", "55-64", "Set the speed limit offset when the speed limit is between 55 and 64 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset3")) + " mph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset3Metric, "Offset3", "90-104", "Set the speed limit offset when the speed limit is between 55 and 64 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset3")) + " kph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset4, "Offset4", "65-99", "Set the speed limit offset when the speed limit is between 65 and 99 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset4")) + " mph";,
+  return std::clamp(v, 0, 99);
+)
+
+ParamController(Offset4Metric, "Offset4", "105-159", "Set the speed limit offset when the speed limit is between 65 and 99 mph.", "../assets/icon_blank.png",
+  return QString::number(params.getInt("Offset4")) + " kph";,
+  return std::clamp(v, 0, 99);
+)
+
 ParamController(PathEdgeWidth, "PathEdgeWidth", "Path Edges", "Customize the path edge width that displays current driving statuses.\n\nDefault is 20% of the total path.\n\nBlue = Navigation\nLight Blue = Always On Lateral\nGreen = Default with 'FrogPilot Colors'\nLight Green = Default with stock colors\nOrange = Experimental Mode Active\nYellow = Conditional Overriden", "../assets/offroad/icon_blank.png",
   return QString::number(params.getInt("PathEdgeWidth")) + "%";,
   return std::clamp(v, 0, 100);
@@ -326,6 +369,34 @@ ParamController(ScreenBrightness, "ScreenBrightness", "Screen Brightness", "Set 
   const int brightness = params.getInt("ScreenBrightness");
   return brightness == 101 ? "Auto" : brightness == 0 ? "Off" : QString::number(brightness) + "%";,
   return std::clamp(v, 0, 101);
+)
+
+ParamController(SLCFallback, "SLCFallback", "SLC Fallback", "Set your preferred fallback method for when there's no speed limit found in either Navigation, OSM, or the car's dashboard.", "../assets/offroad/icon_blank.png",
+  const int fallback = params.getInt("SLCFallback");
+  return fallback == 0 ? "None" : fallback == 1 ? "Experimental Mode" : "Previous Speed Limit";,
+  return v >= 0 ? v % 3 : 2;
+)
+
+ParamController(SLCPriority, "SLCPriority", "SLC Priority", "Set your preferred priority order when deciding what speed limit to use for Speed Limit Controller.", "../assets/offroad/icon_blank.png",
+  const int priority = params.getInt("SLCPriority");
+  return priority == 0 ? "Navigation, Dash, OSM" : 
+         priority == 1 ? "Navigation, OSM, Dash" : 
+         priority == 2 ? "Navigation, OSM" : 
+         priority == 3 ? "Navigation, Dash" : 
+         priority == 4 ? "Navigation" : 
+         priority == 5 ? "OSM, Dash, Navigation" : 
+         priority == 6 ? "OSM, Navigation, Dash" : 
+         priority == 7 ? "OSM, Navigation" : 
+         priority == 8 ? "OSM, Dash" : 
+         priority == 9 ? "OSM" : 
+         priority == 10 ? "Dash, Navigation, OSM" : 
+         priority == 11 ? "Dash, OSM, Navigation" : 
+         priority == 12 ? "Dash, OSM" : 
+         priority == 13 ? "Dash, Navigation" : 
+         priority == 14 ? "Dash" : 
+         priority == 15 ? "Highest" : 
+         "Lowest";,
+  return v >= 0 ? v % 17 : 16;
 )
 
 ParamController(StandardJerk, "StandardJerk", "Jerk Value", "Set the jerk value for the 'Standard Personality'.\n\nValue represents the responsiveness of the brake/gas pedals.\n\nHigher value = Less responsive/more 'relaxed'\n\nStock has a value of 1.0.", "../assets/offroad/icon_blank.png",

selfdrive/ui/qt/onroad.cc
@@ -125,16 +125,23 @@ void OnroadWindow::mousePressEvent(QMouseEvent* e) {
   const QRect speedRect(rect().center().x() - 175, 50, 350, 350);
   const bool isSpeedClicked = speedRect.contains(e->pos());
 
-  if (isMaxSpeedClicked || isSpeedClicked) {
+  // Speed limit offset button
+  const QRect speedLimitRect(7, 250, 225, 225);
+  const bool isSpeedLimitClicked = speedLimitRect.contains(e->pos());
+
+  if (isMaxSpeedClicked || isSpeedClicked || isSpeedLimitClicked) {
     // Check if the click was within the max speed area
     if (isMaxSpeedClicked) {
       reverseCruise = !params.getBool("ReverseCruise");
       params.putBoolNonBlocking("ReverseCruise", reverseCruise);
       paramsMemory.putBoolNonBlocking("FrogPilotTogglesUpdated", true);
     // Check if the click was within the speed text area
-    } else {
+    } else if (isSpeedClicked) {
       speedHidden = !params.getBool("HideSpeed");
       params.putBoolNonBlocking("HideSpeed", speedHidden);
+    } else {
+      showSLCOffset = !params.getBool("ShowSLCOffset");
+      params.putBoolNonBlocking("ShowSLCOffset", showSLCOffset);
     }
     widgetClicked = true;
   // If the click wasn't for anything specific, change the value of "ExperimentalMode"
@@ -477,6 +484,9 @@ AnnotatedCameraWidget::AnnotatedCameraWidget(VisionStreamType type, QWidget* par
   if (params.getBool("ReverseCruise")) {
     reverseCruise = true;
   }
+  if (params.getBool("ShowSLCOffset")) {
+    showSLCOffset = true;
+  }
 
   // Load miscellaneous images
   compass_inner_img = loadPixmap("../assets/images/compass_inner.png", {img_size, img_size});
@@ -528,10 +538,13 @@ void AnnotatedCameraWidget::updateState(const UIState &s) {
   speed *= s.scene.is_metric ? MS_TO_KPH : MS_TO_MPH;
 
   auto speed_limit_sign = nav_instruction.getSpeedLimitSign();
-  speedLimit = nav_alive ? nav_instruction.getSpeedLimit() : 0.0;
+  speedLimit = slcSpeedLimit ? slcSpeedLimit : nav_alive ? nav_instruction.getSpeedLimit() : 0.0;
   speedLimit *= (s.scene.is_metric ? MS_TO_KPH : MS_TO_MPH);
+  if (slcSpeedLimit) {
+    speedLimit = std::round(speedLimit - (showSLCOffset ? slcSpeedLimitOffset : 0));
+  }
 
-  has_us_speed_limit = (nav_alive && speed_limit_sign == cereal::NavInstruction::SpeedLimitSign::MUTCD);
+  has_us_speed_limit = (nav_alive && speed_limit_sign == cereal::NavInstruction::SpeedLimitSign::MUTCD) || slcSpeedLimit;
   has_eu_speed_limit = (nav_alive && speed_limit_sign == cereal::NavInstruction::SpeedLimitSign::VIENNA);
   is_metric = s.scene.is_metric;
   speedUnit =  s.scene.is_metric ? tr("km/h") : tr("mph");
@@ -580,6 +593,9 @@ void AnnotatedCameraWidget::updateState(const UIState &s) {
   obstacleDistanceStock = scene.obstacle_distance_stock;
   roadNameUI = scene.road_name_ui;
   showDriverCamera = scene.show_driver_camera;
+  slcOverridden = scene.speed_limit_overridden;
+  slcSpeedLimit = scene.speed_limit;
+  slcSpeedLimitOffset = scene.speed_limit_offset * (is_metric ? MS_TO_KPH : MS_TO_MPH);
   stoppedEquivalence = scene.stopped_equivalence;
   stoppedEquivalenceStock = scene.stopped_equivalence_stock;
   turnSignalLeft = scene.turn_signal_left;
@@ -622,6 +638,7 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
   p.fillRect(0, 0, width(), UI_HEADER_HEIGHT, bg);
 
   QString speedLimitStr = (speedLimit > 1) ? QString::number(std::nearbyint(speedLimit)) : "–";
+  QString speedLimitOffsetStr = (slcSpeedLimitOffset > 1) ? "+" + QString::number(std::nearbyint(slcSpeedLimitOffset)) : "–";
   QString speedStr = QString::number(std::nearbyint(speed));
   QString setSpeedStr = is_cruise_set ? QString::number(std::nearbyint(setSpeed)) : "–";
 
@@ -685,11 +702,23 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
     p.setPen(QPen(blackColor(), 6));
     p.drawRoundedRect(sign_rect.adjusted(9, 9, -9, -9), 16, 16);
 
-    p.setFont(InterFont(28, QFont::DemiBold));
-    p.drawText(sign_rect.adjusted(0, 22, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("SPEED"));
-    p.drawText(sign_rect.adjusted(0, 51, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("LIMIT"));
-    p.setFont(InterFont(70, QFont::Bold));
-    p.drawText(sign_rect.adjusted(0, 85, 0, 0), Qt::AlignTop | Qt::AlignHCenter, speedLimitStr);
+    p.save();
+    p.setOpacity(slcOverridden ? 0.25 : 1.0);
+    if (showSLCOffset) {
+      p.setFont(InterFont(28, QFont::DemiBold));
+      p.drawText(sign_rect.adjusted(0, 22, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("LIMIT"));
+      p.setFont(InterFont(70, QFont::Bold));
+      p.drawText(sign_rect.adjusted(0, 51, 0, 0), Qt::AlignTop | Qt::AlignHCenter, speedLimitStr);
+      p.setFont(InterFont(50, QFont::DemiBold));
+      p.drawText(sign_rect.adjusted(0, 120, 0, 0), Qt::AlignTop | Qt::AlignHCenter, speedLimitOffsetStr);
+    } else {
+      p.setFont(InterFont(28, QFont::DemiBold));
+      p.drawText(sign_rect.adjusted(0, 22, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("SPEED"));
+      p.drawText(sign_rect.adjusted(0, 51, 0, 0), Qt::AlignTop | Qt::AlignHCenter, tr("LIMIT"));
+      p.setFont(InterFont(70, QFont::Bold));
+      p.drawText(sign_rect.adjusted(0, 85, 0, 0), Qt::AlignTop | Qt::AlignHCenter, speedLimitStr);
+    }
+    p.restore();
   }
 
   // EU (Vienna style) sign
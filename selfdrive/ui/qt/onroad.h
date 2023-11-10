d2fef6245d8362be9e1cdb6bfdbc385894ede2ce
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-11-10 13:21:42 -0700

    Speed limit controller
    Added toggle to control the cruise set speed according to speed limit supplied by OSM, NOO, or the vehicle itself.
    
    Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>
    Co-Authored-By: Efini <19368997+efini@users.noreply.github.com>
    Co-Authored-By: Kumar <36933347+rav4kumar@users.noreply.github.com>

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -18,6 +18,7 @@ const int img_size = (btn_size / 4) * 3;
 
 // FrogPilot global variables
 static bool reverseCruise;
+static bool showSLCOffset;
 static bool speedHidden;
 static double fps;
 
@@ -139,6 +140,7 @@ private:
   bool muteDM;
   bool roadNameUI;
   bool showDriverCamera;
+  bool slcOverridden;
   bool turnSignalLeft;
   bool turnSignalRight;
   double maxAcceleration;
@@ -148,6 +150,8 @@ private:
   float laneWidthRight;
   float obstacleDistance;
   float obstacleDistanceStock;
+  float slcSpeedLimit;
+  float slcSpeedLimitOffset;
   float stoppedEquivalence;
   float stoppedEquivalenceStock;
   int bearingDeg;
