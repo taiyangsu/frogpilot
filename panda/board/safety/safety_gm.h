4d30eb9dc59fc5c153693959877e438b83f7a41c
ServerDummy <63013446+ServerDummy@users.noreply.github.com>
2023-11-30 12:21:16 -0600

    Update safety_gm.h to go brrr more
    faster, trust.

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -17,7 +17,7 @@ const LongitudinalLimits GM_ASCM_LONG_LIMITS = {
 };
 
 const LongitudinalLimits GM_CAM_LONG_LIMITS = {
-  .max_gas = 3400,
+  .max_gas = 4095,
   .min_gas = 1514,
   .inactive_gas = 1554,
   .max_brake = 400,
