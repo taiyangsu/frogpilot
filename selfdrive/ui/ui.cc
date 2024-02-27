3a414ebaeb58fae16070fa603970b6feb2b42f7e
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Vision Turn Speed Controller
    Added toggles for "Vision Turn Speed Control" along with aggressiveness for the speed and sensitivity for the curve itself.
    
    Credit goes to Pfeiferj!
    
    https: //github.com/pfeiferj
    Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -278,6 +278,7 @@ static void update_state(UIState *s) {
       scene.unconfirmed_speed_limit = frogpilotPlan.getUnconfirmedSlcSpeedLimit();
     }
     scene.adjusted_cruise = frogpilotPlan.getAdjustedCruise();
+    scene.vtsc_controlling_curve = frogpilotPlan.getVtscControllingCurve();
   }
   if (sm.updated("liveLocationKalman")) {
     auto liveLocationKalman = sm["liveLocationKalman"].getLiveLocationKalman();
@@ -340,6 +341,7 @@ void ui_update_frogpilot_params(UIState *s) {
   scene.holiday_themes = custom_theme && params.getBool("HolidayThemes");
 
   scene.disable_smoothing_mtsc = params.getBool("DisableMTSCSmoothing");
+  scene.disable_smoothing_vtsc = params.getBool("DisableVTSCSmoothing");
   scene.driver_camera = params.getBool("DriverCamera");
   scene.experimental_mode_via_screen = params.getBool("ExperimentalModeViaScreen") && params.getBool("ExperimentalModeActivation");
   scene.fahrenheit = params.getBool("Fahrenheit");
