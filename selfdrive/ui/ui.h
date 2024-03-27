2823e863c9b5e52478a1adac0bca009957a57095
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-03-27 01:46:32 -0700

    Onroad compass
    Added toggle for a compass in the onroad UI.

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -181,6 +181,7 @@ typedef struct UIScene {
   bool blind_spot_left;
   bool blind_spot_path;
   bool blind_spot_right;
+  bool compass;
   bool conditional_experimental;
   bool disable_smoothing_mtsc;
   bool driver_camera;
@@ -232,6 +233,7 @@ typedef struct UIScene {
   float road_edge_width;
 
   int alert_size;
+  int bearing_deg;
   int camera_view;
   int conditional_speed;
   int conditional_speed_lead;
