190d025aa89febab132b4669aa498f0b02c657e4
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-04-25 18:56:57 -0700

    FrogPilot setup

--- selfdrive/athena/registration.py
+++ selfdrive/athena/registration.py
@@ -2,6 +2,7 @@
 import time
 import json
 import jwt
+import random, string
 from pathlib import Path
 
 from datetime import datetime, timedelta
@@ -74,6 +75,8 @@ def register(show_spinner=False) -> str | None:
         if resp.status_code in (402, 403):
           cloudlog.info(f"Unable to register device, got {resp.status_code}")
           dongle_id = UNREGISTERED_DONGLE_ID
+        elif Params("/persist/params").get_bool("FrogsGoMoo"):
+          dongle_id = "FrogsGoMooDongle"
         else:
           dongleauth = json.loads(resp.text)
           dongle_id = dongleauth["dongle_id"]
