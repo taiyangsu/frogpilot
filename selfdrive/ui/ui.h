8488289e9cfcbff16ab5c0a3b4c7fa8141d51b77
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-11 14:58:28 -0700

    Visuals - Model UI - Road Edges
    Adjust the visual thickness of road edges on your display.
    
    Default is 1/2 of the MUTCD average lane line width of 4 inches.

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -266,6 +266,7 @@ typedef struct UIScene {
   float lat_accel;
   float path_edge_width;
   float path_width;
+  float road_edge_width;
   float speed_jerk;
   float speed_jerk_difference;
   float speed_limit;
