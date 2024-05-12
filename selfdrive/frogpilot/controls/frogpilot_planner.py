7b83765de4f2ddbe9a560516a3e38a8825d5eed4
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-11 20:43:31 -0700

    Controls - Longitudinal Tuning - Acceleration Profile
    Change the acceleration rate to be either sporty or eco-friendly.

--- selfdrive/frogpilot/controls/frogpilot_planner.py
+++ selfdrive/frogpilot/controls/frogpilot_planner.py
@@ -23,6 +23,20 @@ from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_variables import CITY_
 
 GearShifter = car.CarState.GearShifter
 
+# Acceleration profiles - Credit goes to the DragonPilot team!
+                 # MPH = [0., 6.71, 13.4, 17.9, 24.6, 33.6, 44.7, 55.9, 89.5]
+A_CRUISE_MAX_BP_CUSTOM = [0.,    3,   6.,   8.,  11.,  15.,  20.,  25.,  40.]
+
+A_CRUISE_MAX_VALS_ECO = [3.5, 3.2, 2.3, 2.0, 1.15, .80, .58, .36, .30]
+
+A_CRUISE_MAX_VALS_SPORT = [3.5, 3.5, 3.3, 2.8, 1.5, 1.0, 0.75, 0.65, 0.6]
+
+def get_max_accel_eco(v_ego):
+  return interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, A_CRUISE_MAX_VALS_ECO)
+
+def get_max_accel_sport(v_ego):
+  return interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, A_CRUISE_MAX_VALS_SPORT)
+
 class FrogPilotPlanner:
   def __init__(self, CP):
     self.CP = CP
@@ -46,6 +60,15 @@ class FrogPilotPlanner:
 
     v_ego = max(carState.vEgo, 0)
     v_lead = self.lead_one.vLead
+
+    if frogpilot_toggles.acceleration_profile == 1:
+      self.max_accel = get_max_accel_eco(v_ego)
+    elif frogpilot_toggles.acceleration_profile in (2, 3):
+      self.max_accel = get_max_accel_sport(v_ego)
+    elif controlsState.experimentalMode:
+      self.max_accel = ACCEL_MAX
+    else:
+      self.max_accel = get_max_accel(v_ego)
 
     self.base_acceleration_jerk, self.base_speed_jerk = get_jerk_factor(frogpilot_toggles.custom_personalities,
                                                                         frogpilot_toggles.aggressive_jerk_acceleration, frogpilot_toggles.aggressive_jerk_speed,
@@ -91,6 +114,7 @@ class FrogPilotPlanner:
     frogpilotPlan.accelerationJerk = A_CHANGE_COST * float(self.acceleration_jerk)
     frogpilotPlan.accelerationJerkStock = A_CHANGE_COST * float(self.base_acceleration_jerk)
     frogpilotPlan.conditionalExperimental = self.cem.experimental_mode
+    frogpilotPlan.maxAcceleration = self.max_accel
     frogpilotPlan.speedJerk = J_EGO_COST * float(self.speed_jerk)
     frogpilotPlan.speedJerkStock = J_EGO_COST * float(self.base_speed_jerk)
     frogpilotPlan.tFollow = float(self.t_follow)
