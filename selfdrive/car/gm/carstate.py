abf165e83bef1b906334bc8ae51f31cc4c85e560
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-28 23:46:26 -0700

    Controls - Quality of Life - Map Acceleration Profiles To Gears
    Map your acceleration/deceleration profile to your 'Eco' and/or 'Sport' gears.
    
    Co-Authored-By: CHaucke89 <132518562+chaucke89@users.noreply.github.com>
    Co-Authored-By: garrettpall <76917194+garrettpall@users.noreply.github.com>

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -172,6 +172,9 @@ class CarState(CarStateBase):
     # FrogPilot carstate functions
     fp_ret.hasCamera = not (self.CP.flags & GMFlags.NO_CAMERA.value) and self.CP.carFingerprint not in CC_ONLY_CAR
 
+    if self.CP.flags & GMFlags.SPORT_MODE.value:
+      fp_ret.sportGear = pt_cp.vl["SportMode"]["SportMode"] == 1
+
     self.lkas_previously_enabled = self.lkas_enabled
     if self.CP.carFingerprint in SDGM_CAR:
       self.lkas_enabled = cam_cp.vl["ASCMSteeringButton"]["LKAButton"]
@@ -237,6 +240,9 @@ class CarState(CarStateBase):
       ]
       if CP.enableBsm:
         messages.append(("BCMBlindSpotMonitor", 10))
+
+    if CP.flags & GMFlags.SPORT_MODE.value:
+      messages.append(("SportMode", 0))
 
     # Used to read back last counter sent to PT by camera
     if CP.networkLocation == NetworkLocation.fwdCamera:
