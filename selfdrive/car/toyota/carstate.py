nessied/mazda3_2018

Vehicles - Toyota - Cluster Offset

selfdrive/car/toyota/carstate.py
@@ -99,7 +99,7 @@ class CarState(CarStateBase):
     )
     ret.vEgoRaw = mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr])
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
-    ret.vEgoCluster = ret.vEgo * 1.015  # minimum of all the cars
+    ret.vEgoCluster = ret.vEgo * frogpilot_variables.cluster_offset
 
     ret.standstill = abs(ret.vEgoRaw) < 1e-3
 

taiyangsu/frogpilot

selfdrive/car/toyota/carstate.py
@@ -100,7 +100,7 @@ class CarState(CarStateBase):
     )
     ret.vEgoRaw = mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr])
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
-    ret.vEgoCluster = ret.vEgo * 1.015  # minimum of all the cars
+    ret.vEgoCluster = ret.vEgo * frogpilot_toggles.cluster_offset
 
     ret.standstill = abs(ret.vEgoRaw) < 1e-3
 