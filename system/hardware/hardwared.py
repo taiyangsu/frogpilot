dd1f0d343475ab9c1e3db6787e8e2d1401878bd7
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-07-31 19:39:35 -0700

    FrogPilot features - Force device onroad/offroad

--- system/hardware/hardwared.py
+++ system/hardware/hardwared.py
@@ -210,6 +210,8 @@ def hardware_thread(end_event, hw_queue) -> None:
   frogpilot_toggles = FrogPilotVariables.toggles
   FrogPilotVariables.update_frogpilot_params()
 
+  params_memory = Params("/dev/shm/params")
+
   update_toggles = False
 
   while not end_event.is_set():
@@ -343,6 +345,10 @@ def hardware_thread(end_event, hw_queue) -> None:
     should_start = all(onroad_conditions.values())
     if started_ts is None:
       should_start = should_start and all(startup_conditions.values())
+
+    # Handle force offroad/onroad
+    should_start |= params_memory.get_bool("ForceOnroad")
+    should_start &= not params_memory.get_bool("ForceOffroad")
 
     if should_start != should_start_prev or (count == 0):
       params.put_bool("IsEngaged", False)
