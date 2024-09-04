0db1793e6bb58c74f1a27277067a24f5087ff43b
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-09-04 14:05:30 -0700

    Go back to MAC

--- selfdrive/frogpilot/controls/lib/frogpilot_functions.py
+++ selfdrive/frogpilot/controls/lib/frogpilot_functions.py
@@ -284,23 +284,21 @@ def uninstall_frogpilot():
 
   HARDWARE.uninstall()
 
-class WeightedMovingAverageCalculator:
-  def __init__(self, window_size):
-    self.window_size = window_size
-    self.data = []
-    self.weights = np.linspace(1, 3, window_size)
+class MovingAverageCalculator:
+  def __init__(self):
+    self.reset_data()
 
   def add_data(self, value):
-    if len(self.data) == self.window_size:
-      self.data.pop(0)
+    if len(self.data) == 5:
+      self.total -= self.data.pop(0)
     self.data.append(value)
+    self.total += value
 
-  def get_weighted_average(self):
+  def get_moving_average(self):
     if len(self.data) == 0:
       return None
-    weighted_sum = np.dot(self.data, self.weights[-len(self.data):])
-    weight_total = np.sum(self.weights[-len(self.data):])
-    return weighted_sum / weight_total
+    return self.total / len(self.data)
 
   def reset_data(self):
     self.data = []
+    self.total = 0
